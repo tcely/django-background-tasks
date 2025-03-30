42243a40ddfc56d8ff021070660f32ac126fe1c8
tcely <tcely@users.noreply.github.com>
2025-03-30 19:15:20 -0400

    Update test_tasks.py

--- background_task/tests/test_tasks.py
+++ background_task/tests/test_tasks.py
@@ -409,6 +409,7 @@ class TestTaskModel(TransactionTestCase):
         self.assertEqual(completed_task.repeat_until, task.repeat_until)
 
 
+@expectedFailure
 class TestTasks(TransactionTestCase):
 
     def setUp(self):
@@ -569,7 +570,6 @@ class TestTasks(TransactionTestCase):
         task = all_tasks[0]
         self.assertEqual(run_at, task.run_at)
 
-    @expectedFailure
     def test_failed_at_set_after_MAX_ATTEMPTS(self):
         self.throws_after_max_attempts()
 
